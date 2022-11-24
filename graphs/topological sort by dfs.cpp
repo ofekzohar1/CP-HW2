1a011073d7e9f8edac523cc6b4642638e1a92830
Zohar <shtrudel310@gmail.com>
2022-11-24 21:52:02 +0200

    topological sort from pseudo code to code

--- graphs/topological sort by dfs.cpp
+++ graphs/topological sort by dfs.cpp
@@ -1,30 +1,55 @@
-/*
- * Topological sorting by dfs.
+//  NOTE: you need to allocate space and fill g first!
+//  NOTE: we assume indices 1 to n!! (1 based index)
+/* 
+ * Topological sorting by dfs (and cycle detection).
  * - We start dfs from a white node: there's a cycle iff there's a backedge.
  * - We start the order from n-1 to 0. We have the current index
 		and for each member we encounter we assign the index to the node and decrease the index.
 	 This is equivalent to use the reverse-order encountered in the dfs.
    - We don't care about the order between different dfs trees.
- * 
+ *
  */
  
  #define WHITE 0
  #define GREY 1
  #define BLACK 2
- 
-int back_edge_spotted = 0, v[..];
-int od[..], p; // the final order
-void dfs(int a) {
-    if(v[a]==GREY) back_edge_spotted = 1;
-    if(v[a] != WHITE) return;
-    v[a] = GREY;
-    for each neighbor a -> i dfs(i);
-    v[a] = BLACK;
-    od[p] = a; p--;
+
+vector<vector<int>> g; // we assume g is allocated
+vector<int> order;
+vector<int> color;
+int back_edge_spotted = 0;
+int p;
+void dfs(int v) {
+    if(color[v]==GREY) back_edge_spotted = 1;
+    if(color[v] != WHITE) return;
+    color[v] = GREY;
+    for (int neighbor: g[v]) {
+        dfs(neighbor);
+    }
+    color[v] = BLACK;
+    order[p] = v; p--;
 }
-...
-for(i=0; i<n; i++) v[i]=0; //white
-back_edge_spotted=0; p=n-1;
-for(i=0; i<n; i++) if(!v[i])
-dfs(i);
-if(back_edge_spotted) say there is a cycle;
+
+/**
+ * Fills order with the order of the topological sort
+ * Returns true iff no cycle was found.
+ * n is the number of vertices in the graph - assumed to be 1 to n!!!
+ */
+bool topological_sorting(int n) {
+    // init
+    color = vector<int>(n+1);
+    order = vector<int>(n+1);
+    for(int i=1; i<=n; i++) color[i]=WHITE;
+
+    // dfs from each node
+    back_edge_spotted=0; p=n-1;
+    for(int i=1; i<=n; i++) {
+        if(!color[i]) {
+            dfs(i);
+            if(back_edge_spotted) {
+                return false;
+            }
+        };
+    }
+    return true;
+}
